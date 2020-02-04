cc5ee37b2879805a010b7d7fc4266c11b0123b04
Tejashri <tejashri.pawar@capgemini.com>
2020-02-04 11:39:23 +0530

    final commit

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -5,42 +5,42 @@ exports.createPost = (req, res, next) => {
     const post = new Post({
         title: req.body.title,
         content: req.body.content,
-        imagePath:url + '/images/' + req.file.filename,
-        creator:req.userData.userId
+        imagePath: url + '/images/' + req.file.filename,
+        creator: req.userData.userId
     });
-    post.save().then((createdPost)=>{
+    post.save().then((createdPost) => {
         res.status(201).send({
             message: 'Post added successfully',
-            post:{
+            post: {
                 ...createdPost,
                 id: createdPost._id,
             }
         })
     })
-  
+
 }
 
-exports.updatePost = (req,res,next)=> {
+exports.updatePost = (req, res, next) => {
     let imagePath = req.body.imagePath;
-   if(req.file) {
-    const url = req.protocol + '://' + req.get('host');
-    imagePath = url + '/images/' + req.file.filename
-   }
+    if (req.file) {
+        const url = req.protocol + '://' + req.get('host');
+        imagePath = url + '/images/' + req.file.filename
+    }
     const post = new Post({
-        _id:req.body.id,
+        _id: req.body.id,
         title: req.body.title,
-        content:req.body.content,
-        imagePath:imagePath,
-        creator:req.userData.userId
+        content: req.body.content,
+        imagePath: imagePath,
+        creator: req.userData.userId
     })
-    Post.updateOne({_id:req.params.id,creator:req.userData.userId},post)
-    .then((result)=>{
-        if(result.n>0) {
-            res.status(200).json({message:'updated successfully'});
-        } else {
-            res.status(401).json({message:'not authorized'})
-        }
-    })
+    Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post)
+        .then((result) => {
+            if (result.n > 0) {
+                res.status(200).json({ message: 'updated successfully' });
+            } else {
+                res.status(401).json({ message: 'not authorized' })
+            }
+        })
 }
 
 exports.getPosts = (req, res, next) => {
@@ -48,45 +48,43 @@ exports.getPosts = (req, res, next) => {
     const currentPage = +req.query.page;
     let fetchedPost;
     const postQuery = Post.find()
-    if(pageSize && currentPage) {
+    if (pageSize && currentPage) {
         postQuery
-        .skip(pageSize*(currentPage-1))
-        .limit(pageSize);
+            .skip(pageSize * (currentPage - 1))
+            .limit(pageSize);
     }
-   
-    postQuery.then((doc)=>{
+
+    postQuery.then((doc) => {
         fetchedPost = doc;
         return Post.count()
-   })
-   .then((count)=>{
-    res.json({
-        message: 'posts fetched successfully!',
-        posts: fetchedPost,
-        maxPosts: count,
-        newmessage:'just for testing purpose'
-    });
-   })
+    })
+        .then((count) => {
+            res.json({
+                message: 'posts fetched successfully!',
+                posts: fetchedPost,
+                maxPosts: count
+            });
+        })
 }
 
 exports.getPost = (req, res, next) => {
     Post.findById(req.params.id)
-    .then((post)=>{
-        if(post) {
-         res.status(200).json(post);
-        } else {
-            res.status(404).json({message:'Post not found'})
-        }
-    })
-  }
+        .then((post) => {
+            if (post) {
+                res.status(200).json(post);
+            } else {
+                res.status(404).json({ message: 'Post not found' })
+            }
+        })
+}
 
-  exports.deletePost = (req, res, next) => {
-    Post.deleteOne({_id:req.params.id,creator:req.userData.userId})
-    .then((result)=>{
-        if(result.n>0) {
-            res.status(200).json({message:'deletion successfully'});
-        } else {
-            res.status(401).json({message:'not authorized'})
-        }
-    })
- }
- 
+exports.deletePost = (req, res, next) => {
+    Post.deleteOne({ _id: req.params.id, creator: req.userData.userId })
+        .then((result) => {
+            if (result.n > 0) {
+                res.status(200).json({ message: 'deletion successfully' });
+            } else {
+                res.status(401).json({ message: 'not authorized' })
+            }
+        })
+}
